d9f4535177a26fe1919d9ed48a6bf30c4c460d4f
Bartłomiej Piotrowski <b@bpiotrowski.pl>
2021-09-29 09:22:45 +0200

    Ignore "Download sources" failures

--- master/buildbot/flathub_master.py
+++ master/buildbot/flathub_master.py
@@ -1504,7 +1504,7 @@ def create_build_app_factory():
                               haltOnFailure=True,
                               hideStepIf=hide_on_success),
         steps.Trigger(name='Download sources',
-                      haltOnFailure=True,
+                      haltOnFailure=False,
                       schedulerNames=['download-sources'],
                       updateSourceStamp=True,
                       waitForFinish=True,
